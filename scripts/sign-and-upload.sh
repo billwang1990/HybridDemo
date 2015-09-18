c2fb60b700832349aa94a92e428b2a668d75cf35
Yaqing Wang <yaqwang@CNyaqwang.local>
2015-09-18 16:00:51 +0800

    log codesign

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -8,10 +8,13 @@ exit 0
 fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+
+echo $PROVISIONING_PROFILE
+
 OUTPUTDIR="$PWD/build/Release-iphoneos"
-
+echo $OUTPUTDIR
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
-
+ls $OUTPUTDIR
 fir p $OUTPUTDIR/$APPNAME.ipa -T $FIR_APP_TOKEN
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"

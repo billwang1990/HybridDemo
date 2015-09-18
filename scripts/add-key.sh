9940baa7eb39c91ebcd9e1a38ae0dead58cbce9e
Yaqing Wang <yaqwang@CNyaqwang.local>
2015-09-18 17:24:08 +0800

    modify codesign config

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -11,7 +11,19 @@ export PROFILE_NAME="HbridADHoc1"
 echo $KEY_PASSWORD
 echo $PROFILE_NAME
 
+# Create a custom keychain
 security create-keychain -p travis ios-build.keychain
+
+# Make the custom keychain default, so xcodebuild will use it for signing
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
